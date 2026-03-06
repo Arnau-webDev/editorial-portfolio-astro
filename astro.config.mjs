f2a29195ca0a26f3294a3538985633a51012f7ba
Arnau <arnauma16@gmail.com>
2026-03-06 21:12:00 +0100

    fix: use www subdomain as canonical site URL for google crawling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://arnau-mq.dev',
+  site: 'https://www.arnau-mq.dev',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     sitemap(),
